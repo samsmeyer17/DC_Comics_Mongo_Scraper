71644c64dcb6dbc6ee3fdcf0bef56664317ba9a3
Samuel Meyer <sam.smeyer17@gmail.com>
2019-10-30 12:16:57 -0700

    importing packages into server.js file

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ import { Mongoose } from "mongoose";
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI)
+
+import { express } from "express";
+import { expresshandlebars } from "express-handlebars";
+import { logger } from "morgan";
+import { axios } from "axios";
+import { cheerio } from "cheerio";
+import { db } from "./models";
+
