89351114b4e4266c53eaa44ce2e85e8d28fa1dae
Samuel Meyer <sam.smeyer17@gmail.com>
2019-10-31 22:33:05 -0700

    adding scraping method

--- server.js
+++ server.js
@@ -18,11 +18,27 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.connect(MONGODB_URI)
 
 app.get("/scrape", function(req, res) {
-  axios.get("https://www.wsj.com/").then(function(response) {
+  axios.get("https://www.dccomics.com/comics").then(function(response) {
     var $ = cheerio.load(response.data)
 
-    $("article h3").each(function(i, element) {
-      
+    $("div.slide__content").each(function(i, element) {
+      var results = {};
+      var img = $(element).find('a').find('img').attr('src');
+      var title = $(element).find('span').find('a').attr('href')
+
+      results.push({
+        img: img,
+        title: title
+      });
+    });
+    db.articles.update({articles: results}, function(results) {
+
     })
+    console.log(results);
   })
+  res.send("Scrape Complete")
+})
+
+app.listen(MONGODB_URI, function() {
+  console.log("App listening on: " + MONGODB_URI)
 })
