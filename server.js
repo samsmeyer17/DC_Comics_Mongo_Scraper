741feb6a4d1488247a831facd539c160fef04f4b
Samuel Meyer <sam.smeyer17@gmail.com>
2019-10-31 21:54:28 -0700

    adding code for articles and notes models

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-import { Mongoose } from "mongoose";
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI)
-
+import { mongoose } from "mongoose";
 import { express } from "express";
 import { expresshandlebars } from "express-handlebars";
 import { logger } from "morgan";
@@ -11,3 +6,23 @@ import { axios } from "axios";
 import { cheerio } from "cheerio";
 import { db } from "./models";
 
+var app = express();
+
+app.use(logger("dev"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI)
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.wsj.com/").then(function(response) {
+    var $ = cheerio.load(response.data)
+
+    $("article h3").each(function(i, element) {
+      
+    })
+  })
+})
