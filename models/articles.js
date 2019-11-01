d2fd04fe25c267f0d6a3f049a0ef49dcbb332185
Samuel Meyer <sam.smeyer17@gmail.com>
2019-10-31 22:03:24 -0700

    updating models folder

--- models/articles.js
+++ models/articles.js
@@ -1,2 +1,22 @@
 import mongoose from "mongoose";
 
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  note: {
+    type: Schema.Types.ObjectID,
+    ref: "Note"
+  }
+})
+
+var Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
---
+++ models/note.js
@@ -0,0 +1,12 @@
+import { mongoose } from "mongoose";
+
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema({
+  title: String,
+  body: String
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
